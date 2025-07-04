73f75a30929ad0f26dfacb065ce26ea9109c3131
David Molinero <doktor500@users.noreply.github.com>
2025-07-04 21:24:09 +0100

    Create DynamoDB table

--- lib/application-stack.ts
+++ lib/application-stack.ts
@@ -2,12 +2,19 @@ import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { LambdaRestApi } from "aws-cdk-lib/aws-apigateway";
+import * as dynamoDb from 'aws-cdk-lib/aws-dynamodb';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 
 export class ApplicationStack extends Stack {
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
-        const lambdaFunction = new NodejsFunction(this, 'PostPayment', {
+
+        const paymentsTable = new dynamoDb.Table(this, 'payments', {
+            partitionKey: { name: "pk", type: dynamoDb.AttributeType.STRING },
+            billingMode: dynamoDb.BillingMode.PAY_PER_REQUEST,
+        });
+
+        const createPaymentFunction = new NodejsFunction(this, 'CreatePaymentFunction', {
             entry: './lambda/index.ts',
             runtime: lambda.Runtime.NODEJS_20_X,
             handler: 'main',
@@ -15,10 +22,13 @@ export class ApplicationStack extends Stack {
                 externalModules: ['aws-sdk'],
                 minify: true,
             },
+            environment: {
+                PAYMENTS_TABLE_NAME: paymentsTable.tableName,
+            }
         });
 
         new LambdaRestApi(this, 'ApiGwEndpoint', {
-            handler: lambdaFunction,
+            handler: createPaymentFunction,
             restApiName: 'PaymentsApi',
         });
     }
